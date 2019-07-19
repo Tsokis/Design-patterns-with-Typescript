185e4cbb50a934f0e5fb1a249c75e7f7f001c20f
Nikos <nik.ntf1@gmail.com>
2019-07-19 17:23:19 +0300

    User Class

--- src/User.ts
+++ src/User.ts
@@ -1,14 +1,23 @@
 // import faker package for random generated data
+// @ts-ignore
 import faker from 'faker';
+import { Mappable } from "./CustomMap";
 
 
-class User {
+export class User implements Mappable{
     name:string;
     location: {
         lat:number;
         lng:number;
     };
     constructor(){
-
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+    markerContent(): string {
+        return `<strong>User Name: ${this.name}</strong>`;
     }
 }
